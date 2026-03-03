16ed78512083eae1fcaedb435dca80cd1e52e282
Mohammad Faheem <webstereddy3@gmail.com>
2026-03-03 19:04:47 +0530

    fixed CORS Headers Issue

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,10 +7,23 @@ export default auth((req) => {
     const isLoggedIn = !!req.auth;
     const isApiRoute = req.nextUrl.pathname.startsWith("/api");
     const isPublicApiRoute =
-        req.nextUrl.pathname.startsWith("/api/auth");
+        req.nextUrl.pathname.startsWith("/api/auth") ||
+        req.nextUrl.pathname.startsWith("/api/check-status") ||
+        req.nextUrl.pathname.startsWith("/api/seo") ||
+        req.nextUrl.pathname.startsWith("/sdk");
 
     if (isApiRoute && !isPublicApiRoute && !isLoggedIn) {
-        return Response.json({ message: "Unauthorized" }, { status: 401 });
+        return Response.json(
+            { message: "Unauthorized" },
+            {
+                status: 401,
+                headers: {
+                    "Access-Control-Allow-Origin": "*",
+                    "Access-Control-Allow-Methods": "GET, OPTIONS",
+                    "Access-Control-Allow-Headers": "Content-Type",
+                }
+            }
+        );
     }
 
     // Redirect unauthenticated users who try to access other protected routes
